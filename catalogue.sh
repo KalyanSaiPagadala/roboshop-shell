c3e23a6fe4665de6d361449ba345691c981e040c
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-03-11 18:27:21 +0530

    roboshop conf by shell

--- catalogue.sh
+++ catalogue.sh
@@ -31,10 +31,10 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>> $LOGFILE
 VALIDATE $? "disable nodejs"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y &>> $LOGFILE
 VALIDATE $? "enable nodejs:18"
 
 id roboshop
@@ -49,35 +49,35 @@ fi
 mkdir  -p /app
 VALIDATE $? "created /app dir"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
 VALIDATE $? "downloaded the catalogue application"
 
 cd /app 
 
-unzip  -o /tmp/catalogue.zip
+unzip  -o /tmp/catalogue.zip &>> $LOGFILE
 VALIDATE $? "unziped the catalogue application"
 
-npm install 
+npm install &>> $LOGFILE
 VALIDATE $? "dependencies installed "
 
-cp Downloads/dop/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+cp Downloads/dop/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
 VALIDATE $? " copying catalogue service"
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $LOGFILE
 VALIDATE $? "daemon reload"
 
-systemctl enable catalogue
+systemctl enable catalogue &>> $LOGFILE
 VALIDATE $? "enabled catalogue" 
 
-systemctl start catalogue
+systemctl start catalogue &>> $LOGFILE
 VALIDATE $? "started catalogue"
 
-cp Downloads/dop/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp Downloads/dop/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 VALIDATE $? "copied mongo.repo"
 
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>> $LOGFILE
 VALIDATE $? "installation of mongodb-client"
 
-mongo --host $MONGODB_HOST </app/schema/catalogue.js
+mongo --host $MONGODB_HOST </app/schema/catalogue.js &>> $LOGFILE
 VALIDATE $? "loading schema"
 
