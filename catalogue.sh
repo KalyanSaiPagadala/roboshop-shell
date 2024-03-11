7a8e9d6d99d7471b767fa652e0a1cf366471ebed
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-03-11 18:15:39 +0530

    roboshop conf by shell

---
+++ catalogue.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+MONGODB_HOST=mongodb.dawshub.cloud
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf module disable nodejs -y
+VALIDATE $? "disable nodejs"
+
+dnf module enable nodejs:18 -y
+VALIDATE $? "enable nodejs:18"
+
+useradd roboshop
+VALIDATE $? "roboshop user added "
+
+mkdir /app
+VALIDATE $? "created /app dir"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+VALIDATE $? "downloaded the catalogue application"
+
+cd /app 
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "unziped the catalogue application"
+
+npm install 
+VALIDATE $? "dependencies installed "
+
+cp Downloads/dop/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? " copying catalogue service"
+
+systemctl daemon-reload
+VALIDATE $? "daemon reload"
+
+systemctl enable catalogue
+VALIDATE $? "enabled catalogue" 
+
+systemctl start catalogue
+VALIDATE $? "started catalogue"
+
+cp Downloads/dop/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copied mongo.repo"
+
+dnf install mongodb-org-shell -y
+VALIDATE $? "installation of mongodb-client"
+
+mongo --host $MONGODB_HOST </app/schema/catalogue.js
+VALIDATE $? "loading schema"
+
